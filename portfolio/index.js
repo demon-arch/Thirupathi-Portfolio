50a8316b0eee34913870822c35d5f8cc74f7f114
demon-archgit config --global user.email <thiruthirupathi6758@gmail.com>
2024-09-03 13:59:17 +0530

    Initial commit

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,4 +1,20 @@
 
+let p=10000;
+let r=5;
+let t=3;
+let si=(p*r*t)/100;
+console.log("Simple Interest="+si);
+
+let r=14;
+let pi=3.14159
+let area=(pi*r*r);
+console.log("Area of the garden="+area)
+
+// let a=150;
+// let b=200;
+// a=a=b;
+// a=a-b;
+// b=b-a;
 
 
 
